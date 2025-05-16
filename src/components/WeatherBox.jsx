7ece8dbdcc34d95b7d783bec05e83ae6a8670770
SupriyaSharma-git <sharmasupriya0986@gmail.com>
2025-05-17 01:29:08 +0530

    Update WeatherBox.jsx

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -29,22 +29,17 @@ const WeatherBox = () => {
   let icon = "";
   useEffect(() => {
     if (!Location || !APIKey) return;
-
     setData(null); // Reset previous data
-    // setError(null); // Reset errors
 
     const fetchWeather = async () => {
       setLoading(true);
       setError("");
       try {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${Location}&units=metric&appid=${APIKey}`;
-        console.log("Fetching URL:", url);
         const response = await axios.get(url);
-        console.log("API Response:", response.data);
-        // setLoading(true);
         setTimeout(() => {
           setData(response.data); // Properly updates data
-        }); // Delays update by 1.5 seconds
+        });
       } catch (err) {
         console.error("Error fetching data:", err);
         setError("Location Not Found");
@@ -55,9 +50,6 @@ const WeatherBox = () => {
 
     fetchWeather();
   }, [Location]);
-
-  console.log("Data is ", data);
-  console.log("Location is ", Location);
 
   switch (data?.weather?.[0]?.main) {
     case "Clouds":
@@ -162,79 +154,3 @@ const WeatherBox = () => {
   );
 };
 export default WeatherBox;
-
-//   return (
-//     <div className="flex flex-col justify-center items-center backdrop-blur-lg">
-//       <h1 className="text-4xl font-bold text-white m-3">Weather App</h1>
-//       <div className="border border-white rounded-2xl m-2 p-4">
-//         <InputBox />
-//         {data?.name?.toLowerCase() === Location.toLowerCase() ? (
-//           <div>
-//             <div className="flex flex-col justify-center items-center p-7">
-//               <div className="font-bold text-white text-2xl p-3">
-//                 {data.name}, {data.sys.country}
-//               </div>
-//               <div className="flex flex-row justify-center items-center p-4">
-//                 <div className="text-9xl text-white">{icon}</div>
-//                 <div className="text-6xl font-bold ml-3">
-//                   {parseInt(data.main.temp)}
-//                 </div>
-//                 <TbTemperatureCelsius className="text-6xl font-semibold" />
-//               </div>
-//               <div className="text-white font-semibold">
-//                 {data.weather[0].description}
-//               </div>
-//             </div>
-//             <div className="flex flex-col p-4">
-//               <div className="flex flex-row justify-between m-4">
-//                 <div className="flex flex-col">
-//                   <div className="flex flex-row justify-center items-center">
-//                     <BsWater className="text-white text-2xl font-bold mr-2" />
-//                     <div className="text-white text-sm">Humidity</div>
-//                   </div>
-//                   <div className="text-white font-bold">
-//                     {data.main.humidity} %
-//                   </div>
-//                 </div>
-//                 <div className="flex flex-col">
-//                   <div className="flex flex-row justify-center items-center">
-//                     <BsEye className="text-white text-2xl font-bold mr-2" />
-//                     <div className="text-white text-sm">Visibility</div>
-//                   </div>
-//                   <div className="text-white font-bold">
-//                     {data.visibility / 1000} km
-//                   </div>
-//                 </div>
-//               </div>
-//               <div className="flex flex-row justify-between m-4">
-//                 <div className="flex flex-col">
-//                   <div className="flex flex-row justify-center items-center">
-//                     <BsWind className="text-white text-2xl font-bold mr-2" />
-//                     <div className="text-white text-sm">Wind</div>
-//                   </div>
-//                   <div className="text-white font-bold">
-//                     {data.wind.speed} m/s
-//                   </div>
-//                 </div>
-//                 <div className="flex flex-col">
-//                   <div className="flex flex-row justify-center items-center">
-//                     <BsThermometer className="text-white text-2xl font-bold mr-2" />
-//                     <div className="text-white text-sm">Feels Like</div>
-//                   </div>
-//                   <div className="text-white font-bold flex flex-row justify-center items-center">
-//                     {parseInt(data.main.feels_like)}{" "}
-//                     <TbTemperatureCelsius className="font-bold" />
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         ) : (
-//           <div>Not FOund</div>
-//         )}
-//       </div>
-//     </div>
-//   ) : (
-//     <ImSpinner8 className="text-9xl animate-spin text-white" />
-//   );
-// };
